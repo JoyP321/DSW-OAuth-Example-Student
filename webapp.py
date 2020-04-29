c17e5f069d412907b7c395d4987e821e717258b6
JoyP321 <54326509+JoyP321@users.noreply.github.com>
2020-04-29 11:56:39 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -84,7 +84,7 @@ def renderPage1():
 @app.route('/page2')
 def renderPage2():
     if 'user_data' in session:
-        repoData = "This user has " + str(session['user_data']['public_repos'] " public repositories") + #format the user data nicely
+        repoData = "This user has " + str(session['user_data']['public_repos'] " public repositories")  #format the user data nicely
     else:
         repoData = '';
     return render_template('page2.html', data = repoData )
